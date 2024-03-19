1b94c5c8153b386eea39a5bfc799e6ec4504a543
Nalaguri <supraja.nalaguri@capgemini.com>
2024-03-19 13:05:13 +0530

    helloworld

---
+++ DemoController.java
@@ -0,0 +1,36 @@
+package com.cg.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/demo")
+public class DemoController {
+	
+	@GetMapping(value = "/show")
+	String demoMessage() {
+		System.out.println("This is rest api call");
+		return "This is rest api call";
+	}
+	
+	
+	@PostMapping(value = "/save")
+	String demoMessagePost() {
+		System.out.println("This is rest api call");
+		return "This is rest api call for post";
+	}
+	
+	
+	
+	
+	@PutMapping(value = "/delete")
+	String demoMessagePut() {
+		System.out.println("This is rest api call");
+		return "This is rest api call for delete";
+	}
+	
+	
+}
